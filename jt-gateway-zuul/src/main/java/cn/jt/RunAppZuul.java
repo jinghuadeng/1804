fe189b8f5d0301dd16151f49c12b9f21252ff60a
jinghuadeng <840384197@qq.com>
2018-09-12 14:13:45 +0800

    jtspringcloud

---
+++ jt-gateway-zuul/src/main/java/cn/jt/RunAppZuul.java
@@ -0,0 +1,18 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class RunAppZuul {
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppZuul.class, args);
+	}
+}
